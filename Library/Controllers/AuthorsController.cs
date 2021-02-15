73545ae5bd389ea73450de60476e09b4b38cd907
rusimka <rusimkad@gmail.com>
2021-02-15 04:24:38 +0100

    Add project files.

---
+++ Library/Controllers/AuthorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private LibraryDatabaseEntities db = new LibraryDatabaseEntities();
+
+        // GET: Authors
+        public ActionResult Index()
+        {
+            return View(db.Authors.ToList());
+        }
+
+        // GET: Authors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // GET: Authors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,FullName,age")] Authors authors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Authors.Add(authors);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(authors);
+        }
+
+        // GET: Authors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,FullName,age")] Authors authors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(authors).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(authors);
+        }
+
+        // GET: Authors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Authors authors = db.Authors.Find(id);
+            db.Authors.Remove(authors);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        private LibraryDatabaseEntities db = new LibraryDatabaseEntities();
+
+        // GET: Books
+        public ActionResult Index()
+        {
+            var books = db.Books.Include(b => b.Authors);
+            return View(books.ToList());
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            ViewBag.idAuthor = new SelectList(db.Authors, "id", "FullName");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ISBN,title,idAuthor,photo")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(books);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idAuthor = new SelectList(db.Authors, "id", "FullName", books.idAuthor);
+            return View(books);
+        }
+
+        // GET: Books/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idAuthor = new SelectList(db.Authors, "id", "FullName", books.idAuthor);
+            return View(books);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ISBN,title,idAuthor,photo")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(books).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idAuthor = new SelectList(db.Authors, "id", "FullName", books.idAuthor);
+            return View(books);
+        }
+
+        // GET: Books/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Books books = db.Books.Find(id);
+            db.Books.Remove(books);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
