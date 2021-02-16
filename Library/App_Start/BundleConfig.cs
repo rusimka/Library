142c8d0d444baa0167022099f132d5859418faba
rusimka <rusimkad@gmail.com>
2021-02-16 02:09:30 +0100

    added libraries

--- Library/App_Start/BundleConfig.cs
+++ Library/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace Library
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/DataTables/dataTables.bootstrap.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
